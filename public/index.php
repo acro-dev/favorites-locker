1d56f02e1a7ccc078beb06c898157c544869c439
Julien <acro-dev@hotmail.com>
2021-02-06 02:24:06 +0000

    Content: Add the navbar.
    - Minimal navbar with dynamic content if user is
      connected or not.

--- public/index.php
+++ public/index.php
@@ -1,17 +1,42 @@
+<?php
+session_start();
+$_SESSION['username'] = 'Julien';
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta description="Site de gestion de favoris">
     <!-- Stylesheet link -->
     <link rel="stylesheet" href="dist/css/style.min.css">
     <title>Favorites Locker</title>
 </head>
 
 <body>
-    <h1>Favorites Locker</h1>
-
+    <header>
+        <nav>
+            <div class="brand">Favorites-Locker</div>
+            <ul>
+                <?php
+                if (isset($_SESSION['username']) && $_SESSION['username'] != '') {
+                    echo '<li><a href="#">Dashboard</a></li>';
+                    echo '<li><a href="#">Se déconnecter</a></li>';
+                    echo '<li><a href="#">' . $_SESSION['username'] . '</a></li>';
+                } else {
+                    echo '<li><a href="#">Accueil</a></li>';
+                    echo '<li><a href="#">Se connecter</a></li>';
+                    echo '<li><a href="#">S\'inscire</a></li>';
+                }
+                ?>
+            </ul>
+        </nav>
+    </header>
+    <div class="container">
+        <h1>Favorites Locker</h1>
+    </div>
     <!-- Script js -->
     <script src="dist/js/app.js"></script>
 </body>
