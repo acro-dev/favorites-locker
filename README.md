da3dc6944d00f0eb59f51704ab657c38a9432669
Julien <acro-dev@hotmail.com>
2021-02-06 01:23:26 +0000

    Initial commit of the website structure

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # favorites-locker
+
+Website to stock favorites web site and use it like a dashboard.
+We can sort fav link by categories.
---
+++ public/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Stylesheet link -->
+    <link rel="stylesheet" href="dist/css/style.min.css">
+    <title>Favorites Locker</title>
+</head>
+
+<body>
+    <h1>Favorites Locker</h1>
+
+    <!-- Script js -->
+    <script src="dist/js/app.js"></script>
+</body>
+
+</html>
